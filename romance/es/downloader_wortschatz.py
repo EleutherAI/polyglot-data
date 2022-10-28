cd6969d5f40117955bfb4edeaa73edd7653db2ca
pablo <pgonzalez@foqum.io>
2022-10-28 11:46:03 +0200

    Added gitignore, refactored download_wortschatz

--- romance/es/downloader_wortschatz.py
+++ romance/es/downloader_wortschatz.py
@@ -1,52 +1,56 @@
-# Import Module
 import argparse
 import requests
 from bs4 import BeautifulSoup
 
-# Website URL
-URL_CORPORA = 'https://pcai056.informatik.uni-leipzig.de/downloads/corpora/{}'
+# Website URLS
+CORPORA_URL = "https://pcai056.informatik.uni-leipzig.de/downloads/corpora"
+WORTSCHATZ_URL = "https://wortschatz.uni-leipzig.de/en/download"
 
 
-def main(language="Spanish", downloads="1M"):
-    URL = f"https://wortschatz.uni-leipzig.de/en/download/{language}"
-    print("Exploring and downloading files from the page: {}".format(URL))
-    print("Files to download will have the name")
-    # class list set
-    list_of_files = []
-    class_list = set()
+def _extract_download_elements_from_url(url: str):
+    page = requests.get(url)  # Page content from Website URL
+    soup = BeautifulSoup(page.content, "html.parser")  # parse html content
+    return soup.find_all("a", class_="link_corpora_download")
 
-    # Page content from Website URL
-    page = requests.get( URL )
 
-    # parse html content
-    soup = BeautifulSoup( page.content , 'html.parser')
-
-    class_list = soup.find_all("a", class_="link_corpora_download")
-
-    for i in class_list:
+def _files_to_download_from_elements(elements, filter_keyword: str) -> list:
+    list_of_files = list()
+    for i in elements:
         if i.has_attr("data-corpora-file"):
             list_of_files.append(i.attrs["data-corpora-file"])
-
-    # We will download only the files with "1M" in the name (the large ones)
-    files_to_download = [fn for fn in list_of_files if downloads in fn]
-
-    print("Number of files found {}".format(len(files_to_download)))
-
-    for ftd in files_to_download:
-        print("File downloading: {}".format(ftd))
-        response = requests.get(URL_CORPORA.format(ftd))
-        open(ftd, "wb").write(response.content)
+    return [fn for fn in list_of_files if filter_keyword in fn]
 
 
-if __name__ == '__main__':
+def _download_files(files_to_download: list):
+    for file in files_to_download:
+        print(f"Downloading file: {file}")
+        response = requests.get(f"{CORPORA_URL}/{file}")
+        open(file, "wb").write(response.content)
+
+
+def main(language: str = "Spanish", downloads: str = "1M"):
+    language_url = f"https://wortschatz.uni-leipzig.de/en/download/{language}"
+    print(f"Exploring and downloading files from: {language_url}")
+
+    download_elements = _extract_download_elements_from_url(language_url)
+    files_to_download = _files_to_download_from_elements(
+        download_elements, filter_keyword=downloads
+    )
+    print(f"Found {len(files_to_download)} files")
+    _download_files(files_to_download)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=f"Downloads sentences from https://wortschatz.uni-leipzig.de/en/download in the given language."
     )
-    parser.add_argument('--language',
+    parser.add_argument(
+        "--language",
         default="Spanish",
-        help='Language to download.',
+        help="Language to download.",
     )
-    parser.add_argument('--downloads',
+    parser.add_argument(
+        "--downloads",
         default="1M",
         help='Number of sentences to download. Defaults to "1M"',
     )
