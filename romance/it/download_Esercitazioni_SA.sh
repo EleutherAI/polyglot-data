62bdfbde2d797ac3149297e4a37040b75e592c3e
Gangsss <rlarkdals7@gmail.com>
2023-02-27 17:02:14 +0900

    add shell for dwonload Esercitazioni_SA

---
+++ romance/it/download_Esercitazioni_SA.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/RacheleSprugnoli/Esercitazioni_SA.git
+mv Esercitazioni_SA/dataset/MultiEmotions-It.tsv .
+rm -rf Esercitazioni_SA
+mkdir Esercitazioni_SA
+mv MultiEmotions-It.tsv Esercitazioni_SA
